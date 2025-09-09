deab7dc44440fc2fc5ad2d168b90fce34eb4dd70
José Miguel <josemiguel.escribano.ruiz@gmail.com>
2025-09-09 14:50:30 +0200

    add: routers basic_auth_user and jwt_auth_user

--- routers/jwt_auth_user.py
+++ routers/jwt_auth_user.py
@@ -1,4 +1,4 @@
-from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status
 from pydantic import BaseModel
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 import jwt
@@ -9,7 +9,7 @@ ALGORITHM_ENCODING = "HS256"
 ACCESS_TOKEN_DURATION = 1 # minuto
 SECRET = "SYGCJLKAXKHSLXaSLÑaXÑKDVHSL" # Clave secreta para firmar el token
 
-app = FastAPI()
+router = APIRouter(tags=["JWT Auth User"]) # Etiqueta para la documentación
 oauth2 = OAuth2PasswordBearer(tokenUrl="login")
 crypt = CryptContext(schemes=["bcrypt"])
 
@@ -68,7 +68,7 @@ async def current_user(user: User = Depends(auth_user)):
                             detail="Usuario inactivo.") 
     return user
 
-@app.post("/login")
+@router.post("/login")
 async def login(form: OAuth2PasswordRequestForm = Depends()):
     user_db = users_db.get(form.username)
     if not user_db:
@@ -87,6 +87,6 @@ async def login(form: OAuth2PasswordRequestForm = Depends()):
 
     return {"access_token": jwt.encode(access_token, SECRET, algorithm=ALGORITHM_ENCODING), "token_type": "bearer"}
 
-@app.get("/users/me")
+@router.get("/users/me")
 async def me(user: User = Depends(current_user)):
     return user
