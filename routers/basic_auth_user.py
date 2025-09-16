d9ab3a208b15a5f79ad96de307ddfdd1d70cfbb2
José Miguel <josemiguel.escribano.ruiz@gmail.com>
2025-09-16 16:30:38 +0200

    add: etiqueta basic auth user

--- routers/basic_auth_user.py
+++ routers/basic_auth_user.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from pydantic import BaseModel
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
-router = APIRouter()
+router = APIRouter(tags=["Basic Auth User"])
 oauth2 = OAuth2PasswordBearer(tokenUrl="login")
 
 class User(BaseModel):
